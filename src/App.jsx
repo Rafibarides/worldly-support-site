9dbb94db0bc5b4dab916b4344d890bf6b42c5d91
Rafibarides <rafibaridesmusic@gmail.com>
2025-03-26 14:54:41 -0400

    pushing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
